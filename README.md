4a4119f92affa195f4b65cb4510df5ac3967d239
Hari Dara <haridara@gmail.com>
2023-01-30 22:26:16 +0530

    Adding the script with basic usage

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # ffmpeg-slide-gen
+
 Combine slides and audio to produce video using ffmpeg
+
+## What is this repo about?
+
+This repo provides a Python script to automate the execution of `ffmpeg` in order to produce a presentation video. It takes the following as input:
+- A sequence of slide images
+- The audio of the presentation
+- Timestamp for each of the slides
+
+The script then computes the durations for each slide to produce a *concat* file and runs it through `ffmpeg` along with audio to generate a presentation video.
+
+This makes it easy to record the audio of a presentation (the easiest part) and later combine it with relevant presentation with ease.
+
+## Requirements
+
+Tested with Python 3.9.1 but may work with other Python 3 versions. Requires a few Python libraries that can be installed (virtualenv is preferred) using the command:
+
+```
+pip install -r requirements.txt
+```
+
+Also depends on `ffmpeg` and `mediainfo` commands so make sure to have them in the shell `PATH`.
+
+## Usage
+
+Run the `ffmpeg-slide-gen.py` script with `--help` to see the usage. All options are required (except `--help` itself).
+
+```
+Usage: ffmpeg-slide-gen.py [OPTIONS]
+
+  Generate FFMpeg demux concat file from specified slides and timestamps. See:
+  https://superuser.com/a/619843/26006
+
+Options:
+  -c, --slide-count TEXT          Number of slides  [required]
+  -i, --slides FILE...            List of image filenames as an ordered
+                                  sequence  [required]
+  -s, --slide-ts, --ts <INTEGER INTEGER>...
+                                  Pair of integers: slide timestamp
+                                  [required]
+  -a, --audio-file FILE           Specify the path to the audio file
+                                  [required]
+  -o, --video-out TEXT            Specify the path for the output video file
+                                  [required]
+  --help                          Show this message and exit.
+```
+
+## Overall Approach
+
+- Export presentation as images
+    - A PPT file can be opened in Keynote and all slides can be exported to a folder, say /tmp/proj
+    - A PDF can be exported to images using `ghostscript`, e.g., `gs -sDEVICE=jpeg -sOutputFile=page-%03d.jpg -r1280x720 -f file.pdf`
+- Open the presentation and play the audio to identify the timestamps.
+- Whenever a change in slide is observed, take a note of the slide number and timestamp (in seconds)
+- Use the above timestamps to construct the command-line to produce final video
+
+## Sample command-line
+
+```
+python ffmpeg-slide-gen.py -a /tmp/proj/audio.mp4 -o /tmp/proj/video.mp4 -c $(echo /tmp/proj/*.jpeg | wc -w) -i /tmp/proj/*.jpeg \
+  -s 1 0 \
+  -s 2 5 \
+  -s 3 35 \
+  -s 4 40 \
+  -s 5 105 \
+  -s 6 476 \
+  -s 7 719 \
+  -s 8 832 \
+  -s 9 892 \
+  -s 10 904 \
+  -s 11 965 \
+  -s 12 979 \
+  -s 13 1068 \
+  -s 14 1167 \
+  -s 15 1178 \
+  -s 14 1261 \
+  -s 16 1310 \
+  -s 15 1406 \
+  -s 17 2139
+```
+
+NOTE: the above command relies on the shell wildcard expansion to order them sequentially, which works as expected for the slides exported from Keynote on a Mac computer.
+
+As can be seen above, slide numbers can be repeated and in any order, in case the presenter doesn't go through them sequentially or goes back and forth.
---
+++ ffmpeg-slide-gen.py
@@ -0,0 +1,68 @@
+import sys
+from typing import Tuple
+from tempfile import NamedTemporaryFile
+
+import click
+from sh import mediainfo, ffmpeg
+
+
+def with_dynamic_narg(cnt_opt, tgt_opt):
+    class DynamicNArgSetter(click.Command):
+        def parse_args(self, ctx, args):
+            ctx.resilient_parsing = True
+            parser = self.make_parser(ctx)
+            opts, _, _ = parser.parse_args(args=list(args))
+            if cnt_opt in opts:
+                for p in self.params:
+                    if isinstance(p, click.Option) and p.name == tgt_opt:
+                        p.nargs = int(opts[cnt_opt])
+
+            ctx.resilient_parsing = False
+            return super().parse_args(ctx, args)
+
+    return DynamicNArgSetter
+
+
+@click.command(cls=with_dynamic_narg('slide_count', 'slides'))
+#@click.option("--total-duration", "--td", "-t", type=click.INT, required=True, help="Total duration of the video in seconds")
+@click.option("--slide-count", "-c", required=True, help="Number of slides")
+@click.option("--slides", "-i", nargs=0, required=True, type=click.Path(exists=True, dir_okay=False, resolve_path=True), help="List of image filenames as an ordered sequence")
+@click.option("--slide-ts", "--ts", "-s", "slide_timestamps", multiple=True, required=True, type=(int, int), help="Pair of integers: slide timestamp")
+@click.option("--audio-file", "-a", required=True, type=click.Path(exists=True, dir_okay=False, resolve_path=True), help="Specify the path to the audio file")
+@click.option("--video-out", "-o", required=True, help="Specify the path for the output video file")
+def main(slide_count, slides, slide_timestamps, audio_file, video_out):
+    """
+    Generate FFMpeg demux concat file from specified slides and timestamps.
+    See: https://superuser.com/a/619843/26006
+    """
+    slide_idx = dict(enumerate(isinstance(slides, Tuple) and slides or [slides], start=1))
+    total_duration = int(mediainfo('--Inform=Audio;%Duration%', audio_file)) / 1000
+
+    slide_durs = []
+    last_ts = -1
+    for slide_no, slide_ts in slide_timestamps:
+        if slide_no not in slide_idx:
+            raise click.UsageError(f"Invalid slide number: {slide_no}")
+        if slide_ts < 0 or slide_ts > total_duration:
+            raise click.UsageError(f"Invalid timestamp: {slide_ts} for slide number: {slide_no}, timestamp must be between 0 and {total_duration}")
+        if slide_ts <= last_ts:
+            raise click.UsageError(f"Invalid timestamp: {slide_ts} for slide number: {slide_no}, timestamp must be greater than the last timestamp: ${last_ts}")
+
+        slide_durs.append((slide_no, slide_ts - (last_ts >= 0 and last_ts or 0)))
+        last_ts = slide_ts
+    slide_durs.append((slide_no, None))
+
+    with NamedTemporaryFile(mode="w") as concat_file:
+        print(f"Writing concat file to the tempfile: {concat_file.name}")
+        concat_file.write("ffconcat version 1.0\n")
+        for slide_no, slide_dur in slide_durs:
+            concat_file.write(f"file {slide_idx[slide_no]}\n")
+            if slide_dur is not None:
+                concat_file.write(f"duration {slide_dur}\n")
+
+        print("Generating slide video")
+        ffmpeg("-f", "concat", "-safe", "0", "-i", concat_file.name, "-i", audio_file, "-c:a", "copy", "-c:v", "libx264", "-pix_fmt", "yuv420p", "-vf", "fps=10, scale=1280:720", video_out, _out=sys.stdout, _err=sys.stderr, _in=sys.stdin)
+
+
+if __name__ == '__main__':
+    main()
