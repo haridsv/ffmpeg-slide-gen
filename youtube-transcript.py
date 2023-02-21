2fe75767f687fddbd9bd5831bf89ae9f399d0d44
Hari Dara <haridara@gmail.com>
2023-02-21 10:06:51 +0530

    Split by word count option

--- youtube-transcript.py
+++ youtube-transcript.py
@@ -5,10 +5,10 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 
 # Return aggregated lines at the specified interval duration
-def lines(transcript, int_dur=60):
+def lines_by_dur(transcript, int_dur):
     cur_st = 0
-    next_st = int_dur
     cur_text = ""
+    next_st = int_dur
     line_iter = iter(transcript.fetch())
     while True:
         try:
@@ -26,21 +26,51 @@ def lines(transcript, int_dur=60):
             break
         else:
             cur_st = line_map["start"]
-            next_st = cur_st + int_dur
             cur_text = line_map["text"]
+            next_st = cur_st + int_dur
+
+
+def lines_by_words(transcript, word_count):
+    cur_st = 0
+    cur_text = ""
+    cur_wc = 0
+    line_iter = iter(transcript.fetch())
+    while True:
+        try:
+            line_map = next(line_iter)
+        except StopIteration:
+            line_map = None
+        new_wc = line_map and len(line_map['text'].split()) or 0
+        if line_map and (cur_wc + new_wc) < word_count:
+            cur_st = cur_st or line_map['start']
+            cur_text = cur_text + " " + line_map['text']
+            cur_wc += new_wc
+            continue
+        if line_map and (cur_wc + new_wc) >= word_count:
+            res_map = {'start': cur_st, 'text': cur_text}
+            yield res_map
+        if not line_map:
+            break
+        else:
+            cur_st = line_map["start"]
+            cur_text = line_map["text"]
+            cur_wc = len(cur_text.split())
 
 
 @click.command()
 @click.option("--interval-duration", "--id", "-d", default=60, help="Interval duration for generating timestamps, use 0 to disable and produce raw output")
+@click.option("--word-count", "--wc", "-w", default=0, help="Number of words for generating timestamps, applicable when --interval-duration=0")
 @click.option("--timestamp-in-seconds", "--tis", "-s", is_flag=True, default=False, help="Specify to see duration in seconds instead of min:sec")
 @click.argument("video-id")
-def main(interval_duration, timestamp_in_seconds, video_id):
+def main(interval_duration, timestamp_in_seconds, word_count, video_id):
     # Retrieve the available transcripts
     transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
     # Just use the first transcript, let it raise an exception if none exist.
     transcript = next(iter(transcript_list))
     print("<html><body><table>")
-    lines_iter = interval_duration <= 0 and transcript.fetch() or lines(transcript, interval_duration)
+    lines_iter = (interval_duration <= 0 and word_count <= 0) and transcript.fetch() or (
+        interval_duration > 0 and lines_by_dur(transcript, interval_duration)) or lines_by_words(transcript, word_count)
+
     for line_map in lines_iter:
         link_to_tstmp = f"https://youtu.be/{video_id}?t={line_map['start']}"
         if timestamp_in_seconds:
